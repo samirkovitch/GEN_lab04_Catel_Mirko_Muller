a0064e6c6ac3f1b6e40acf1eeb5854acae2069d3
raph126 <simon.mirkovitch@heig-vd.ch>
2019-05-10 11:06:08 +0200

    Extraction de fonction : getContents

--- src/main/Orders.java
+++ src/main/Orders.java
@@ -16,4 +16,37 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    public String getContents() {
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < getOrdersCount(); i++) {
+            Order order = getOrder(i);
+            sb.append("{\"id\": " + order.getOrderId() + ", \"products\": [");
+
+            for (int j = 0; j < order.getProductsCount(); j++) {
+                Product product = order.getProduct(j);
+
+                sb.append("{\"code\": \"" + product.getCode() + "\", \"color\": \"" + product.getColor() + "\", ");
+
+                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+                    sb.append("\"size\": \"" + product.getSize() + "\", ");
+                }
+
+                sb.append("\"price\": " + product.getPrice() + ", \"currency\": \"" + product.getCurrency() + "\"}, ");
+            }
+
+            if (order.getProductsCount() > 0) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("]}, ");
+        }
+
+        if (getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
--- src/main/OrdersWriter.java
+++ src/main/OrdersWriter.java
@@ -1,42 +1,9 @@
 
 public class OrdersWriter {
-    private Orders orders;
+    Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
-    }
-
-    public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{\"id\": " + order.getOrderId() + ", \"products\": [");
-            
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{\"code\": \"" + product.getCode() + "\", \"color\": \"" + product.getColor() + "\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"" + product.getSize() + "\", ");
-                }
-
-                sb.append("\"price\": " + product.getPrice() + ", \"currency\": \"" + product.getCurrency() + "\"}, ");
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
     }
 
 }
