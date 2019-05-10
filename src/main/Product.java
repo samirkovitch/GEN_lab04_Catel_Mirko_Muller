5853c7ca106a1b18ad230b3da0de340c94867f68
raph126 <simon.mirkovitch@heig-vd.ch>
2019-05-10 09:34:40 +0200

    Extraction de fonction : getSizeFor

--- src/main/Product.java
+++ src/main/Product.java
@@ -34,4 +34,23 @@ public class Product {
     public String getCurrency() {
         return currency;
     }
+
+    public String getSizeFor() {
+        switch (getSize()) {
+            case 1:
+                return "XS";
+            case 2:
+                return "S";
+            case 3:
+                return "M";
+            case 4:
+                return "L";
+            case 5:
+                return "XL";
+            case 6:
+                return "XXL";
+            default:
+                return "Invalid Size";
+        }
+    }
 }
